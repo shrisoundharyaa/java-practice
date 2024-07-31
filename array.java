456de705173b6a4a167d24f352c09841b64f4c2c
shrisoundharyaa <shrisoundharyaa@gmail.com>
2024-07-31 21:43:51 +0530

    practiced a loop in array to get input from user

---
+++ array.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+
+// class array{
+//     public static void main(String args[]){
+//      int[] scores  = new int[5];
+//      Scanner scan = new Scanner(System.in);
+//      scores[0] = scan.nextInt();
+//      scores[1] = scan.nextInt();
+//      scores[2] = scan.nextInt();
+//      scores[3] = scan.nextInt();
+//      scores[4] = scan.nextInt();
+     
+
+//      System.out.println(scores[0]+scores[1]+scores[2]+scores[3]+scores[4]);
+//     }
+// }
+
+import java.util.Scanner;
+ class array{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int[] marks = new int[5];
+        for(int i=0;i<5;i++){
+            marks[i] = scan.nextInt();
+        }
+        for(int i=0;i<5;i++){
+            System.out.print(marks[i]);
+        }
+    }
+ }
