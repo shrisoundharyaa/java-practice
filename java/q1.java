8b9abc2c6e6e9957b1c0483c6c3905d5145a22d4
shrisoundharyaa <shrisoundharyaa@gmail.com>
2024-08-17 21:51:47 +0530

    practice some problems in java

---
+++ java/q1.java
@@ -0,0 +1,22 @@
+package java;
+
+class q1{
+    static int x = 10;
+    static{
+        System.out.println(x);
+        m1();
+    }
+    public static void main(String[] args){
+        m1();
+         System.out.println("main");
+    }
+
+    public static void m1(){
+        System.out.println(y);
+    }
+    static{
+        System.out.println("ssd");
+    }
+    static int  y = 20;
+    
+}
